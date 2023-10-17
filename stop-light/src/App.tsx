e1594cdf9215f55f8b0079fa5eb8f57c4d50acb5
Omar Baldi <omar.baldi.business@gmail.com>
2023-10-17 11:10:10 +0200

    Integrated custom hook into entrypoint app file and added style logic to highlight active stop light

--- stop-light/src/App.tsx
+++ stop-light/src/App.tsx
@@ -1,7 +1,32 @@
 import './App.css';
+import { lightsColors } from './constants/lightsColors';
+import { useStopLight } from './hooks/useStopLight';
+
+const commonStopLightStyle: React.CSSProperties = {
+  height: '50px',
+  width: '50px',
+  borderRadius: '50%',
+  backgroundColor: 'grey',
+};
 
 function App() {
-  return <></>;
+  const activeStopLightColor = useStopLight();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      {lightsColors.map((color, index) => (
+        <div
+          key={`color-${color}-#${index}`}
+          style={{
+            ...commonStopLightStyle,
+            ...(activeStopLightColor === color && {
+              backgroundColor: color,
+            }),
+          }}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
