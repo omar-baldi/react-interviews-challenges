b055cfdc630c9c4cfb36ca8eb1cff7e85e05a0d8
Omar Baldi <omar.baldi.business@gmail.com>
2023-10-30 14:29:00 +0100

    Implemented logic to update single checkbox value clicked and reset all subsequent children recursively

--- checked-tree/src/App.tsx
+++ checked-tree/src/App.tsx
@@ -84,16 +84,21 @@ const initialStateArray = [
  * - change id value to be more consistent (unique for every element)
  * - implement logic to set to unchecked all children elements
  * when parent is unchecked
+ *
+ * !NOTE: to refactor: want to recursively update all parents only if current element is set to unchecked
  */
 function CheckboxTreeElement({
   label,
   checked,
   coordinatesCheckboxElement,
+  isParentElementChecked,
   onCheckboxElementClick,
 }: Omit<CheckboxElement, 'children'> & {
   coordinatesCheckboxElement: number[];
+  isParentElementChecked?: boolean;
   onCheckboxElementClick(
-    coordinatesCheckboxClicked: number[]
+    coordinatesCheckboxClicked: number[],
+    isParentElementChecked?: boolean
   ): (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   const checkboxTreeElementStyle = {
@@ -110,7 +115,10 @@ function CheckboxTreeElement({
         type='checkbox'
         id='checkbox-tree'
         checked={checked}
-        onChange={onCheckboxElementClick(coordinatesCheckboxElement)}
+        onChange={onCheckboxElementClick(
+          coordinatesCheckboxElement,
+          isParentElementChecked
+        )}
       />
     </div>
   );
@@ -119,12 +127,15 @@ function CheckboxTreeElement({
 function CheckboxElementsTree({
   arr,
   parentCheckboxElementCoordinate = [],
+  isParentElementChecked,
   singleCheckboxElementClick,
 }: {
   arr: CheckboxElement[];
   parentCheckboxElementCoordinate?: number[];
+  isParentElementChecked?: boolean;
   singleCheckboxElementClick(
-    coordinatesCheckboxClicked: number[]
+    coordinatesCheckboxClicked: number[],
+    isParentElementChecked?: boolean
   ): (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   return (
@@ -137,6 +148,7 @@ function CheckboxElementsTree({
             <CheckboxTreeElement
               {...v}
               coordinatesCheckboxElement={checkboxCoordinates}
+              isParentElementChecked={isParentElementChecked}
               onCheckboxElementClick={singleCheckboxElementClick}
             />
 
@@ -144,6 +156,7 @@ function CheckboxElementsTree({
               <CheckboxElementsTree
                 arr={v.children}
                 parentCheckboxElementCoordinate={checkboxCoordinates}
+                isParentElementChecked={v.checked}
                 singleCheckboxElementClick={singleCheckboxElementClick}
               />
             )}
@@ -185,16 +198,96 @@ function App() {
     });
   }
 
-  function singleCheckboxElementClick(coordinatesCheckboxClicked: number[]) {
+  function updateAllChildrenCheckedValuesRecursively(
+    arr: CheckboxElement[],
+    coordinatesCheckboxClicked: number[],
+    checkboxClickedCheckedValue: boolean,
+    shouldResetCheckedValue?: boolean
+  ): CheckboxElement[] {
+    const [firstCoordinate, ...restCoordinates] = coordinatesCheckboxClicked;
+
+    return arr.map((v, i) => {
+      return {
+        ...v,
+        ...(shouldResetCheckedValue && {
+          checked: false,
+          ...(Array.isArray(v.children) &&
+            v.children.length > 0 && {
+              children: updateAllChildrenCheckedValuesRecursively(
+                v.children,
+                [],
+                checkboxClickedCheckedValue,
+                shouldResetCheckedValue
+              ),
+            }),
+        }),
+        ...(i === firstCoordinate && {
+          /**
+           * @description
+           * If there is only one last coordinate left, that means
+           * that we have reached the target element and therefore
+           * we need to updated the "checked" value.
+           *
+           * * Tricky part
+           *
+           * Starting from here, I need to reset all of the
+           * subsequent children elements to be "false".
+           */
+          ...(coordinatesCheckboxClicked.length === 1 && {
+            checked: checkboxClickedCheckedValue,
+          }),
+          ...(Array.isArray(v.children) &&
+            v.children.length > 0 && {
+              children: updateAllChildrenCheckedValuesRecursively(
+                v.children,
+                restCoordinates,
+                checkboxClickedCheckedValue,
+                coordinatesCheckboxClicked.length === 1
+              ),
+            }),
+        }),
+      };
+    });
+  }
+
+  /**
+   * !NOTE: to split the following function into 2 main functions
+   * - one function to update single checkbox element
+   * - another function to check whether children values need to be reset
+   */
+  function singleCheckboxElementClick(
+    coordinatesCheckboxClicked: number[],
+    isParentElementChecked?: boolean
+  ) {
     return function (e: React.ChangeEvent<HTMLInputElement>) {
       const { checked: updatedCheckedValue } = e.target;
 
       setCheckboxElementsArr((prevCheckboxElementsArr) => {
-        const updatedCheckboxesElements = updateAllParentsCheckedValueRecursively(
-          prevCheckboxElementsArr,
-          updatedCheckedValue,
-          coordinatesCheckboxClicked
-        );
+        const updatedCheckboxesElements =
+          typeof isParentElementChecked === 'undefined' || isParentElementChecked
+            ? /**
+               * @description
+               * If parent element checked that means I need to uncheck
+               * the current checkbox clicked and do the same for the
+               * children of the checkbox clicked.
+               */
+
+              updateAllChildrenCheckedValuesRecursively(
+                checkboxElementsArr,
+                coordinatesCheckboxClicked,
+                updatedCheckedValue
+              )
+            : /**
+               * @description
+               * Instead, if the parent element is not checked,
+               * I need to recursively iterate through the parents
+               * and set all of them as checked.
+               */
+              updateAllParentsCheckedValueRecursively(
+                prevCheckboxElementsArr,
+                updatedCheckedValue,
+                coordinatesCheckboxClicked
+              );
 
         return updatedCheckboxesElements;
       });
