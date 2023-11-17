ddc674a3c90c57f6bbd9df2177cb4e280868ae9e
Omar Baldi <omar.baldi.business@gmail.com>
2023-11-17 13:28:51 +0100

    Refactoring of logic to reset previous selected incorrect pairs and handled logic to render message if user won

--- country-capital-matching/src/components/CountryCapitalGame.tsx
+++ country-capital-matching/src/components/CountryCapitalGame.tsx
@@ -14,6 +14,9 @@ type ButtonMapValue = {
 export default function CountryCapitalGame({ data }: Props) {
   const previousButtonClicked = useRef<{ id: string; pairId: string } | null>(null);
   const [buttonsMap, setButtonsMap] = useState<Map<string, ButtonMapValue>>(new Map());
+  const [userWon, setUserWon] = useState<boolean>(false);
+
+  //TODO: create custom hook to handle state update logic + "useReducer" hook
 
   useEffect(() => {
     const v = Object.entries(data).reduce((acc, labels, index) => {
@@ -35,12 +38,15 @@ export default function CountryCapitalGame({ data }: Props) {
   function handleButtonClick(buttonClickedId: string, buttonClickedPairId: string) {
     if (!previousButtonClicked.current) {
       setButtonsMap((prevButtonsMap) => {
-        const updatedButtonsMap = new Map(prevButtonsMap);
-
-        updatedButtonsMap.set(buttonClickedId, {
-          ...(updatedButtonsMap.get(buttonClickedId) as ButtonMapValue),
-          status: 'SELECTED',
-        });
+        const updatedButtonsMap = new Map(
+          [...prevButtonsMap].map(([id, rest]) => [
+            id,
+            {
+              ...rest,
+              status: buttonClickedId === id ? 'SELECTED' : 'NOT_SELECTED',
+            } as ButtonMapValue,
+          ])
+        );
 
         return updatedButtonsMap;
       });
@@ -63,6 +69,10 @@ export default function CountryCapitalGame({ data }: Props) {
         updatedButtonsMap.delete(prevButtonClickedId);
         updatedButtonsMap.delete(buttonClickedId);
 
+        if (updatedButtonsMap.size <= 0) {
+          setUserWon(true);
+        }
+
         return updatedButtonsMap;
       });
     } else {
@@ -84,6 +94,10 @@ export default function CountryCapitalGame({ data }: Props) {
     }
 
     previousButtonClicked.current = null;
+  }
+
+  if (userWon) {
+    return <h3>Congratulations!</h3>;
   }
 
   return (
