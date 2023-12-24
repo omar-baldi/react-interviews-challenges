4c8c1e242687831e28b6b2a8b38eb410c640d40d
Omar Baldi <omar.baldi.business@gmail.com>
2023-12-24 13:42:56 +0100

    Refactoring of helper function to accept optional number param for nested presents probability

--- nested-presents/src/constants.ts
+++ nested-presents/src/constants.ts
@@ -1,4 +1,5 @@
 export const MAX_ITERATIONS_ALLOWED = 5;
 export const MAX_PRESENTS_AMOUNT = 4;
 export const DEFAULT_PRESENT_IMAGE_SIZE = 200;
+export const DEFAULT_NESTED_PRESENTS_PROBABILITY = 0.4;
 export const BASE_IMAGE_URL = 'https://cdn2.iconfinder.com/data/icons/christmas-46/64';
--- nested-presents/src/helpers/presents.ts
+++ nested-presents/src/helpers/presents.ts
@@ -1,5 +1,6 @@
 import {
   BASE_IMAGE_URL,
+  DEFAULT_NESTED_PRESENTS_PROBABILITY,
   DEFAULT_PRESENT_IMAGE_SIZE,
   MAX_ITERATIONS_ALLOWED,
   MAX_PRESENTS_AMOUNT,
@@ -15,19 +16,28 @@ export function hasNestedPresents(present: Present): present is Required<Present
   return Array.isArray(present.nestedPresents) && present.nestedPresents.length > 0;
 }
 
-//TODO: add probability optional number prop (3%) -> 0.3 (for boolean property below)
-export function generateNestedPresents(iteration = 1): Present[] {
+/**
+ * @param probabilityNestedPresents - probability of generating nested presents (e.g: 0.4 for 40% probability)
+ * @param iteration
+ */
+export function generateNestedPresents({
+  probabilityNestedPresents = DEFAULT_NESTED_PRESENTS_PROBABILITY,
+  iteration = 1,
+}: {
+  probabilityNestedPresents?: number;
+  iteration?: number;
+} = {}): Present[] {
   const presentsAmount = generateRandomNumber(2, MAX_PRESENTS_AMOUNT);
 
   return [...Array(presentsAmount)].map<Present>(() => {
-    const shouldCreateNestedPresents = Math.random() < 0.4;
+    const shouldCreateNestedPresents = Math.random() < probabilityNestedPresents;
     const presentSize = Math.floor(DEFAULT_PRESENT_IMAGE_SIZE / iteration);
 
     return {
       size: presentSize,
       ...(iteration < MAX_ITERATIONS_ALLOWED &&
         shouldCreateNestedPresents && {
-          nestedPresents: generateNestedPresents(iteration + 1),
+          nestedPresents: generateNestedPresents({ iteration: iteration + 1 }),
         }),
     };
   });
