ed9714305b6a50a1240c64f0ff86c43b9f30211c
Omar Baldi <omar.baldi.business@gmail.com>
2024-01-03 22:05:18 +0100

    Implemented functionality to remove last circle drawn

--- dot-screen/src/App.tsx
+++ dot-screen/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 //TODO: move to "helpers" and unit test this
@@ -9,8 +9,24 @@ function isCanvasAvailable(
   return ctx instanceof CanvasRenderingContext2D;
 }
 
+//TODO: move to "types" folder
+type CircleCoordinates = {
+  x: number;
+  y: number;
+};
+
+//TODO: move to "types" folder
+type CirclesCoordinates = {
+  added: CircleCoordinates[];
+  removed: CircleCoordinates[];
+};
+
 function App() {
   const canvasElementRef = useRef<HTMLCanvasElement>(null);
+  const [circlesCoordinates, setCirclesCoordinates] = useState<CirclesCoordinates>({
+    added: [],
+    removed: [],
+  });
 
   function drawCircle(e: MouseEvent) {
     const circleCoordinateX = e.offsetX;
@@ -28,6 +44,27 @@ function App() {
     }
   }
 
+  function removeLastCircleAdded() {
+    const ctx = canvasElementRef.current?.getContext('2d');
+
+    if (isCanvasAvailable(ctx) && circlesCoordinates.added.length > 0) {
+      ctx.clearRect(0, 0, 1000, 500);
+
+      const updatedAddedCircles = circlesCoordinates.added.slice(0, -1);
+      for (const circle of updatedAddedCircles) {
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, 10, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+      }
+
+      setCirclesCoordinates((prevCirclesCoordinates) => ({
+        ...prevCirclesCoordinates,
+        added: updatedAddedCircles,
+      }));
+    }
+  }
+
   useEffect(() => {
     const canvasElement = canvasElementRef.current;
     canvasElement?.addEventListener('click', drawCircle);
@@ -39,6 +76,17 @@ function App() {
 
   return (
     <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '1rem',
+        }}
+      >
+        <button onClick={removeLastCircleAdded}>Undo</button>
+        <button onClick={undefined}>Redo</button>
+      </div>
+
       <canvas
         ref={canvasElementRef}
         //TODO: replace magic number with constants variable
