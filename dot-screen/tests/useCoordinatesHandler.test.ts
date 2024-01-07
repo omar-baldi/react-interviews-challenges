fcc1cfa21f34248b00c10d8a17bc725d5e8f6f39
Omar Baldi <omar.baldi.business@gmail.com>
2024-01-07 22:29:54 +0100

    Assert that circle coordinates added is updated with new coordinates of circle drawn

--- dot-screen/tests/useCoordinatesHandler.test.ts
+++ dot-screen/tests/useCoordinatesHandler.test.ts
@@ -1,11 +1,31 @@
 /* eslint-disable */
 import { useCoordinatesHandler } from '@/hooks/useCoordinatesHandler';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 
 describe('useCoordinatesHandler', () => {
   it('should initialize circle coordinates "added" and "removed" with empty arrays', () => {
     const { result } = renderHook(() => useCoordinatesHandler());
     expect(result.current.circleCoordinates.added).toEqual([]);
     expect(result.current.circleCoordinates.added).toEqual([]);
+  });
+
+  it('should update circle coordinates "added" when drawing a new circle', () => {
+    const { result } = renderHook(() => useCoordinatesHandler());
+
+    const mockEvent = {
+      clientX: 100,
+      clientY: 200,
+      currentTarget: {
+        getBoundingClientRect() {
+          return {
+            left: 0,
+            top: 0,
+          };
+        },
+      },
+    } as any;
+
+    act(() => result.current.drawCircle(mockEvent));
+    expect(result.current.circleCoordinates.added.length).toBe(1);
   });
 });
